2660695838708df190a469fa6c33ea1bbbb7fce3
MarcinMac <jimrajnor@gmail.com>
2018-09-17 22:27:20 +0200

    make floating label and repair some bootstrap outline

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,10 +2,39 @@
 import { withFormsy } from 'formsy-react';
 import React from 'react';
 
-class Formsy extends React.Component {
+class Input extends React.Component {
   constructor(props) {
     super(props);
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.activateField = this.activateField.bind(this);
+    this.disableField = this.disableField.bind(this);
     this.changeValue = this.changeValue.bind(this);
+    this.state = {
+      inputValue: '',
+      fieldActive: false
+    };
+  }
+  // to activate the input field while typing
+  activateField() {
+    this.setState({
+      fieldActive: true
+    })
+  }
+  // to deactivate input only if it's empty
+  disableField(e) {
+    if (e.target.value === "") {
+      this.setState({
+        fieldActive: false
+      })
+    }
+  }
+  // to update the changes in the input and activate it
+  updateInputValue(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
+    this.activateField(e);
+    e.preventDefault();
   }
 
   changeValue(event) {
@@ -14,6 +43,7 @@ class Formsy extends React.Component {
     // Important: Don't skip this step. This pattern is required
     // for Formsy to work.
     this.props.setValue(event.currentTarget.value);
+    this.updateInputValue(event)
   }
 
   render() {
@@ -21,16 +51,26 @@ class Formsy extends React.Component {
     const errorMessage = this.props.getErrorMessage();
 
     return (
-      <div>
+      <div className="form-group mt-4 position-relative">
+        <label htmlFor={this.props.name} className={this.state.fieldActive ? "label field-active" : "label"}>
+          {this.props.label}
+        </label>
         <input
+          className="floating-label form-control"
+          id={this.props.name}
           onChange={this.changeValue}
-          type="text"
+          type={this.props.type || 'text'}
+          name={this.props.name}
           value={this.props.getValue() || ''}
+          hidden={this.props.hidden}
+          onFocus={this.activateField}
+          onBlur={this.disableField}
         />
+        <small className="form-text text-muted">{this.props.placeholder}</small>
         <span>{errorMessage}</span>
       </div>
     );
   }
 }
 
-export default withFormsy(Formsy);
+export default withFormsy(Input);
