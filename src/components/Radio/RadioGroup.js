863760d305c204e04565d14348a1922fde860591
MarcinMac <jimrajnor@gmail.com>
2018-09-23 13:33:46 +0200

    adding RadioGroup

---
+++ src/components/Radio/RadioGroup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Radio from 'components/Radio/Radio'
+
+const RadioGroup = (props) => {
+  const { radios } = props
+
+  return (
+    <GridContainer spacing={0} className="form-group mt-1">
+      {radios.map((prop, key) => {
+        return (
+          <GridItem xs={Number(prop.grid) || props.grid} sm={Number(prop.gridSM) || props.gridSM} key={key}>
+            <Radio icon={prop.icon || ''} values={props.values} size={prop.size || props.size} onBlur={props.onBlur} onChange={prop.onchange || props.onChange} label={prop.label} value={prop.value} name={props.name}></Radio>
+          </GridItem>
+        )
+      })}
+    </GridContainer>
+  );
+}
+
+export default RadioGroup;
