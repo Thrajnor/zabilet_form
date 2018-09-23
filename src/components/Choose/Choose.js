79283dca237c3aafffc5aa128297be2ae4314599
MarcinMac <jimrajnor@gmail.com>
2018-09-23 13:34:51 +0200

    dismembering big return statment and adding formik

--- src/components/Choose/Choose.js
+++ src/components/Choose/Choose.js
@@ -3,146 +3,157 @@ import React from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 
-// @material-ui/icons
-import Face from "@material-ui/icons/Face";
-import Chat from "@material-ui/icons/Chat";
-import Build from "@material-ui/icons/Build";
+//  @material-ui/icons components
+// import ReportProblem from '@material-ui/icons/ReportProblem'
+// import LocalAirport from '@material-ui/icons/LocalAirport'
+// import Settings from '@material-ui/icons/Settings'
+// import Accessibility from '@material-ui/icons/Accessibility'
+// import Cloud from '@material-ui/icons/Cloud'
+// import FlightTakeoff from '@material-ui/icons/FlightTakeoff'
+// import WbIncandescent from '@material-ui/icons/WbIncandescent'
+
 // core components
-import CustomTabs from "components/CustomTabs/CustomTabs.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
 import tabsStyle from "assets/jss/material-kit-react/views/componentsSections/tabsStyle.jsx";
 
-import Input from 'components/Input/Input'
-import Radio from 'components/Radio/Radio'
+import RadioGroup from 'components/Radio/RadioGroup'
+
 
 class Choose extends React.Component {
-  state = {
-    tabClicked: false,
-    option: 'delay',
-    selectedOption: ''
-  }
-  selectHandler = (value) => {
-    this.setState({ tabClicked: value }, function () {
-      if (value === 0) {
-        this.setState({ option: 'delay' })
-      } else if (value === 1) {
-        this.setState({ option: 'boarding' })
-      } else if (value === 2) {
-        this.setState({ option: 'dismiss' })
-      }
-    })
+  whichRadioGroup = () => {
+    if (this.props.values.whatHappend === 'delay') {
+      return (
+        <div>
+          <h4 className=''>Jaki był powód spóźnienia do miejsca docelowego?</h4>
+          <small className="form-text text-muted pb-2 mb-3">
+            Jeśli Twoj samolot spóźnił się więcej niż 3 godziny załugujesz na odszkodowanie!
+          </small>
+          <RadioGroup
+            onBlur={this.props.onBlur}
+            onChange={this.props.onChange}
+            compensation={this.props.compensation}
+            name='why'
+            grid={12}
+            gridSM={6}
+            radios={[
+              {
+                label: 'Problemy Techniczne',
+                value: 'techProblem',
+              },
+              {
+                label: 'Wpływ innych lotów',
+                value: 'otherFlights',
+              },
+              {
+                label: 'Strajk',
+                value: 'strike',
+              },
+              {
+                label: 'Problemy na lotnisku',
+                value: 'airPortProblems',
+              },
+              {
+                label: 'Złe warunki pogodowe',
+                value: 'badWeather',
+              },
+              {
+                label: 'Nie podano powodu',
+                value: 'noCauseGiven',
+              },
+              {
+                label: 'Nie pamiętam',
+                value: 'dontRemember',
+              },
+              {
+                label: 'Inny',
+                value: 'Other',
+              }
+            ]} />
+        </div>
+      )
+    } else if (this.props.values.whatHappend === 'boardingRefused') {
+      return (
+        <div>
+          <h4 className=''>Czy dobrowolnie zrezygnowałaś/eś z lotu?</h4>
+          <small className="form-text text-muted pb-2 mb-3">
+            Jeśli zrezygnowałaś/eś ze swojej rezerwacji w zamian za bilet na późniejszy lot lub inne bonusy od linii lotniczej nie będzie należało Ci się odszkodowanie.
+          </small>
+          <RadioGroup
+            onBlur={this.props.onBlur}
+            onChange={this.props.onChange}
+            name='why'
+            compensation={this.props.compensation}
+            grid={12}
+            gridSM={6}
+            radios={[
+              {
+                label: 'Tak',
+                value: 'ownWill',
+              },
+              {
+                label: 'Nie',
+                value: 'forced',
+              }
+            ]} />
+        </div>
+      )
+
+    } else if (this.props.values.whatHappend === 'dismissed') {
+      return (
+        <div>
+          <h4 className=''>Jaki był powód odwołania lotu?</h4>
+          <small className="form-text text-muted pb-2 mb-3">
+            Jeśli odwołanie nastąpiło mniej niż 14 dni przed wylotem zasługujesz do odszkodowanie!
+          </small>
+          <RadioGroup
+            onBlur={this.props.onBlur}
+            onChange={this.props.onChange}
+            name='why'
+            compensation={this.props.compensation}
+            grid={12}
+            gridSM={6}
+            radios={[
+              {
+                label: 'Problemy Techniczne',
+                value: 'techProblem',
+              },
+              {
+                label: 'Wpływ innych lotów',
+                value: 'otherFlights',
+              },
+              {
+                label: 'Strajk',
+                value: 'strike',
+              },
+              {
+                label: 'Problemy na lotnisku',
+                value: 'airPortProblems',
+              },
+              {
+                label: 'Złe warunki pogodowe',
+                value: 'badWeather',
+              },
+              {
+                label: 'Nie podano powodu',
+                value: 'noCauseGiven',
+              },
+              {
+                label: 'Nie pamiętam',
+                value: 'dontRemember',
+              },
+              {
+                label: 'Inny',
+                value: 'Other',
+              }
+            ]} />
+        </div>
+      )
+    }
   }
   render() {
-    const { classes } = this.props;
+
     return (
       <div>
-        <span className='d-none'>
-          <Input type='text' name='whatHappend' value={this.state.option} class='d-none' hidden='hidden' required></Input>
-        </span>
-        <CustomTabs
-          headerColor="primary"
-          onClick={this.selectHandler}
-          tabs={[
-            {
-              tabName: "Opóźnienie",
-              tabIcon: Face,
-              value: 'delay',
-              tabContent: (
-                <div>
-                  <p className={[classes.textCenter, 'mb-2 pb-1'].join(' ')}>
-                    Jaki był powód spóźnienia do miejsca docelowego?
-                  </p>
-                  <GridContainer spacing={0} className="form-group mt-1">
-                    <GridItem xs={6}>
-                      <Radio label='Problemy Techniczne' value='techProblem' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Wpływ innych lotów' value='otherFlights' name='why' checked='true'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Strajk' value='strike' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Problemy na lotnisku' value='airPortProblems' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Złe warunki pogodowe' value='badWeather' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Nie podano powodu' value='noCauseGiven' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Nie pamiętam' value='dontRemember' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Inny' value='Other' name='why'></Radio>
-                    </GridItem>
-                  </GridContainer>
-                </div>
-              )
-            },
-            {
-              tabName: "Nie przyjęli mnie na pokład",
-              tabIcon: Chat,
-              value: 'boarding',
-              tabContent: (
-                <div>
-                  <h6 className={classes.textCenter}>Czy dobrowolnie zrezygnowałaś/eś z lotu?</h6>
-                  <p className={[classes.textCenter, 'mb-2 pb-1'].join(' ')}>
-                    Jeśli zrezygnowałaś/eś ze swojej rezerwacji w zamian za bilet na późniejszy lot lub inne bonusy od linii lotniczej nie będzie należało Ci się odszkodowanie.
-                  </p>
-                  <GridContainer spacing={0} className="form-group mt-1">
-                    <GridItem xs={6}>
-                      <Radio label='Tak' value='yes' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Nie' value='no' name='why'></Radio>
-                    </GridItem>
-                  </GridContainer>
-                </div>
-              )
-            },
-            {
-              tabName: "Odwołany lot",
-              tabIcon: Build,
-              value: 'dismiss',
-              tabContent: (
-                <div>
-                  <p className={[classes.textCenter, 'mb-2 pb-1'].join(' ')}>
-                    Jaki był powód odwołania lotu?
-                  </p>
-                  <GridContainer spacing={0} className="form-group mt-1">
-                    <GridItem xs={6}>
-                      <Radio label='Problemy Techniczne' value='techProblem' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Wpływ innych lotów' value='otherFlights' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Strajk' value='strike' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Problemy na lotnisku' value='airPortProblems' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Złe warunki pogodowe' value='badWeather' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Nie podano powodu' value='noCauseGiven' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Nie pamiętam' value='dontRemember' name='why'></Radio>
-                    </GridItem>
-                    <GridItem xs={6}>
-                      <Radio label='Inny' value='Other' name='why'></Radio>
-                    </GridItem>
-                  </GridContainer>
-                </div>
-              )
-            }
-          ]}
-        />
+        {this.whichRadioGroup()}
       </div>
     );
   }
