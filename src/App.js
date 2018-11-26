4c79d9214602e3171f8c422bba5b0f86923c6d29
MarcinMac <jimrajnor@gmail.com>
2018-11-26 13:42:46 +0100

    add some testing values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Form from "./Form"
-import './App.css'
+import React from 'react';
+import Form from './Form';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Form></Form>
+      <div className="container ml-popup-window">
+        <Form />
       </div>
     );
   }
