c6620c621b38d7df8e197e43713397fb0f3df476
MarcinMac <jimrajnor@gmail.com>
2018-11-26 13:45:12 +0100

    hop over container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Form from './Form';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('formatka'));
+ReactDOM.render(<Form />, document.getElementById('formatka'));
 // registerServiceWorker();
